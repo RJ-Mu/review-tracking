4ece50f3fa1eebe7008bbd6bad71247129269905
Rishèn Mungroop <rjmungroop@gmail.com>
2026-07-22 10:31:42 +0300

    Update
    different style, added pollen radar, increased forecasts

---
+++ sw.js
@@ -0,0 +1,25 @@
+var CACHE = "wx-v1";
+var ASSETS = [
+  "./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest",
+  "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./apple-touch-icon.png"
+];
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); })
+    .then(function () { return self.skipWaiting(); }));
+});
+self.addEventListener("activate", function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.map(function (k) { if (k !== CACHE) return caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+self.addEventListener("fetch", function (e) {
+  var url = new URL(e.request.url);
+  if (e.request.method !== "GET" || url.origin !== self.location.origin) return;
+  e.respondWith(caches.match(e.request).then(function (hit) {
+    return hit || fetch(e.request).then(function (res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return res;
+    });
+  }));
+});
